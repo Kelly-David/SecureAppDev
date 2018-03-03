e5914dbe8e2e614ac86516d866514c3fa5fa24b4
kelly-david <davidkelly.tlf@gmail.com>
2018-03-03 12:34:34 +0000

    _crypt added to password rest

--- forgotpassword.php
+++ forgotpassword.php
@@ -47,7 +47,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($stmt = mysqli_prepare($link, $sql)) {
                 mysqli_stmt_bind_param($stmt, "ss", $param_token, $param_dob);
                 $param_token = $token;
-                $param_dob = $dob;
+                $param_dob = _crypt($dob);
                 if (mysqli_stmt_execute($stmt)) {
                     mysqli_stmt_store_result($stmt);
                     if (mysqli_stmt_num_rows($stmt) == 1) {
@@ -113,7 +113,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Time
                 $param_lastLogin = getTime();
                 // Set email
-                $param_email = $email;
+                $param_email = _crypt($email);
                 if (mysqli_stmt_execute($stmt)) {
                     logger("GENERATE TOKEN", $email, "forgotpassword.php", "SUCCESS");
                     // Success - bind the email to the session
